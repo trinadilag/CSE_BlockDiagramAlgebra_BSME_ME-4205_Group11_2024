38f2203d21c5aa6523276c9cdee7c03f46eb6837
Geliane <159035926+Geliane@users.noreply.github.com>
2024-05-21 10:05:09 +0800

    Add files via upload

---
+++ CSE_Lab3_No1_Diagram.m
@@ -0,0 +1,24 @@
+%clear
+clear
+clc
+close all
+
+%% Define G1, G2, G3, H1, H2, H3
+
+G1_num = [1];
+G1_den = [1 0 0];
+
+G2_num = [1];
+G2_den = [1 1];
+
+G3_num = [1];
+G3_den = [1 0];
+
+H1_num = [1];
+H1_den = [1 0];
+
+H2_num = [1];
+H2_den = [1 -1];
+
+H3_num = [1];
+H3_den = [1 -2];
